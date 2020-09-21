3384211c0e495524457108f389c222fbee6f535a
MaK <mkmarek91@gmail.com>
2020-09-21 07:41:13 +0200

    Add config for eks cluster

--- main.tf
+++ main.tf
@@ -1,7 +1,19 @@
 module "eks-jx" {
   source              = "jenkins-x/eks-jx/aws"
   vault_user          = var.vault_user
+  cluster_name = "jx-eks"
   is_jx2              = false
   cluster_version     = "1.16"
+  enable_key_name = true
   enable_worker_group = false
+  enable_logs_storage = true
+  enable_reports_storage = true
+  enable_repository_storage = true
+  force_destroy = true
+  iops = 100
+  key_name = "jx-eks"
+  max_node_count = 3
+  node_group_disk_size = "35"
+  node_machine_type = "t3.medium"
+  production_letsencrypt = true
 }
